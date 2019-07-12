649428fbaa864a832e0c7f7b3b6ed0600cf34a62
Andrey Gridyaev <agridyaev@gmail.com>
2019-07-12 12:38:17 +0300

    add BrowserMob requests interception

--- src/main/java/DriverManager.java
+++ src/main/java/DriverManager.java
@@ -1,22 +1,36 @@
 import com.codeborne.selenide.Configuration;
 import io.github.bonigarcia.wdm.ChromeDriverManager;
+import net.lightbody.bmp.BrowserMobProxy;
+import net.lightbody.bmp.BrowserMobProxyServer;
+import net.lightbody.bmp.client.ClientUtil;
+import net.lightbody.bmp.core.har.Har;
+import net.lightbody.bmp.core.har.HarEntry;
+import net.lightbody.bmp.proxy.CaptureType;
+import org.openqa.selenium.Proxy;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
+import java.net.URL;
 import java.util.HashMap;
+import java.util.List;
 
+import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 /**
  * Created by agr on 5/8/18.
  */
-public class DriverManager {
-
+class DriverManager {
     /**
      * Initialize webdriver and open home page.
      * @param   startPage    Starting page for test
      */
-    public static void initDriver(String startPage, String downloadFolderPath) {
+    static BrowserMobProxy initDriver(URL startPage, String downloadFolderPath) {
+        BrowserMobProxy proxy = new BrowserMobProxyServer();
+        proxy.start(0);
+        Proxy seleniumProxy = ClientUtil.createSeleniumProxy(proxy);
+
         ChromeDriverManager.getInstance().setup();
 
         HashMap<String, Object> chromePreferences = new HashMap<>();
@@ -27,9 +41,13 @@ public class DriverManager {
 
         Configuration.browserCapabilities = new DesiredCapabilities();
         Configuration.browserCapabilities.setCapability(ChromeOptions.CAPABILITY, chromeOptions);
+        Configuration.browserCapabilities.setCapability(CapabilityType.PROXY, seleniumProxy);
         Configuration.browser = System.getProperty("selenide.browser");
         Configuration.pageLoadStrategy = "normal";
 
-        open(startPage);
+        proxy.enableHarCaptureTypes(CaptureType.REQUEST_CONTENT, CaptureType.RESPONSE_CONTENT);
+
+        open(startPage.toString());
+        return proxy;
     }
 }
--- src/test/java/DownloadTest.java
+++ src/test/java/DownloadTest.java
@@ -1,3 +1,10 @@
+import com.codeborne.selenide.proxy.SelenideProxyServer;
+import net.lightbody.bmp.BrowserMobProxy;
+import net.lightbody.bmp.BrowserMobProxyServer;
+import net.lightbody.bmp.core.har.Har;
+import net.lightbody.bmp.core.har.HarEntry;
+import net.lightbody.bmp.filters.RequestFilter;
+import org.testng.Assert;
 import org.testng.ITestContext;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -5,20 +12,33 @@ import org.testng.annotations.Test;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
 
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.WebDriverRunner.getSelenideProxy;
 import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 
 /**
  * Created by a.gridyaev on 5/8/18.
  */
 public class DownloadTest {
+    private BrowserMobProxy proxy;
+    private URL testUrl;
+    {
+        try {
+            testUrl = new URL("https://www.adam.com.au/support/blank-test-files");
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+    }
 
     private static final String downloadFolderPath = System.getProperty("downloadFolderPath");
 
     @BeforeMethod
     public void setUp() {
-        DriverManager.initDriver("https://www.adam.com.au/support/blank-test-files", downloadFolderPath);
+        proxy = DriverManager.initDriver(testUrl, downloadFolderPath);
     }
 
     @AfterMethod
@@ -37,19 +57,23 @@ public class DownloadTest {
         System.out.println(String.format("Downloaded file path: %s", extraSmallFileMD5.getAbsolutePath()));
         System.out.println(String.format("Downloaded file size: %d bytes", extraSmallFileMD5.length()));
 
-        extraSmallFileMD5.delete();
+        Assert.assertTrue(extraSmallFileMD5.delete());
     }
 
     @Test(enabled = true)
-    public void DownloadFileByClick() throws FileNotFoundException, InterruptedException {
+    public void DownloadFileByClick() {
         File downloadFolder = new File(downloadFolderPath);
 
         System.out.println(String.format("Directory \"%s\" exists: %s", downloadFolder.getAbsolutePath(), downloadFolder.exists()));
         System.out.println(String.format("Path \"%s\" is directory: %s", downloadFolder.getAbsolutePath(), downloadFolder.isDirectory()));
 
+        proxy.newHar(testUrl.getHost());
         $("table tbody tr:nth-child(1) td.last a").click();
-
-        Thread.sleep(5000);
+        Har har = proxy.getHar();
+        List<HarEntry> entries = har.getLog().getEntries();
+        for (HarEntry entry : entries) {
+            System.out.println(entry.getRequest().getUrl());
+        }
 
         File expectedDownloadedFile = new File(downloadFolder.getAbsolutePath() + "/SpeedTest_16MB.md5");
         File actualDownloadedFile = new File(System.getProperty("user.home") + "/Downloads/" + "SpeedTest_16MB.md5");
@@ -57,6 +81,6 @@ public class DownloadTest {
         System.out.println(String.format("Expected downloaded file \"%s\" exists: %s", expectedDownloadedFile.getAbsoluteFile(), expectedDownloadedFile.exists()));
         System.out.println(String.format("Actual downloaded file \"%s\" exists: %s", actualDownloadedFile.getAbsoluteFile(), actualDownloadedFile.exists()));
 
-        actualDownloadedFile.delete();
+        Assert.assertTrue(actualDownloadedFile.delete());
     }
 }
